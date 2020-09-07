b06ca77ed148e892a05b805fc1539239640f91cd
Jhonny <jhonnyrivas1999@gmail.com>
2020-09-07 14:18:44 -0400

    blog-categoy view

--- webproject/blog/urls.py
+++ webproject/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^blog/$', views.blog, name='blog')
+    re_path(r'^blog/$', views.blog, name='blog'),
+    re_path(r'^blog/(?P<pk>\d+)/$', views.topics_view, name='topics'),
 ]
--- webproject/blog/views.py
+++ webproject/blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-from .models import Categories
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Categories, Post
 
 # Create your views here.
 
 def blog(request):
     categories = Categories.objects.all()
     return render(request, 'blog/blog.html', {'categories':categories})
+
+def topics_view(request, pk):
+    category = get_object_or_404(Categories, pk=pk)
+    return render(request, 'blog/topics_view.html', {'category':category})
+
