9841c7e1299c7db2f2e86365c978ecf436a31491
Jhonny <jhonnyrivas1999@gmail.com>
2020-09-07 14:53:14 -0400

    v services- service view services: service view

--- webproject/services/urls.py
+++ webproject/services/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    re_path(r'^servicios/$', views.service_view, name='services')
+    re_path(r'^servicios/$', views.services_view, name='services'),
+    re_path(r'^servicios/(?P<pk>\d+)/$', views.service_view, name='service'),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- webproject/services/views.py
+++ webproject/services/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Services
 
 # Create your views here.
 
-def service_view(request):
+def services_view(request):
     services = Services.objects.all()
-    return render(request, 'services/service_view.html', {'services':services})
+    return render(request, 'services/services_view.html', {'services':services})
+
+def service_view(request, pk):
+    service = get_object_or_404(Services, pk=pk)
+    return render(request, 'services/service_view.html', {'service':service})
